98a67477fe768b9e57051957c698225dd9cec539
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-01-17 12:47:51 +0300

    Abstract Renderer class

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,10 @@
+class Renderer {
+    public:
+        virtual void initialize() = 0;
+        virtual void draw_frame() = 0;
+        virtual void terminate() = 0;    
+};
+
 enum GameState {
     initialize,
     running,
