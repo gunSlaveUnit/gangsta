9b1d5cbc6345f72c2bd59cd244552e7e5d62a2ea
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-01-17 21:28:51 +0300

    Set how interpret triangle vertices

--- src/main.cpp
+++ src/main.cpp
@@ -91,6 +91,9 @@ class OpenGLRenderer : public Renderer {
 
             glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+            glEnableVertexAttribArray(0);
+
             glfwSwapBuffers(window);
             glfwPollEvents();
         }
