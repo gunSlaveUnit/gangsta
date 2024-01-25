3ec449b883d18d11acf2651bc8a97862236e1279
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-01-25 21:19:36 +0300

    Reconfigured vertex attributes: added color

--- src/main.cpp
+++ src/main.cpp
@@ -101,8 +101,11 @@ void draw_frame() {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     auto program = shader_program({
         shader(GL_VERTEX_SHADER, "../shaders/vertex.glsl"),
