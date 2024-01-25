4a3bae2db8cd1ec988eae1fa2b46c46fe992db40
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-01-25 21:16:00 +0300

    Color layout in vertex shader

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,7 +1,11 @@
 #version 450 core
 
 layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 vertex_color;
+
+out vec3 pixel_color;
 
 void main() {
 	gl_Position = vec4(position, 1.0);
+	pixel_color = vertex_color;
 }
