45eac8da8d8c533de14d00330049dd0016d59315
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-01-28 22:25:35 +0300

    No indices

--- src/main.cpp
+++ src/main.cpp
@@ -60,10 +60,6 @@ float vertices[] = {
     -0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 0.0f,
     -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
 };
-unsigned int indices[] = {
-    0, 1, 3,
-    1, 2, 3,
-}; 
 
 GLFWwindow *window;
 
@@ -133,15 +129,8 @@ void draw_frame() {
     uint_fast32_t vbo;
     glGenBuffers(1, &vbo);
 
-    uint_fast32_t ebo;
-    glGenBuffers(1, &ebo);
-
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -172,8 +161,8 @@ void draw_frame() {
 
     glBindVertexArray(vao);
 
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+    
     glfwSwapBuffers(window);
     glfwPollEvents();
 }
