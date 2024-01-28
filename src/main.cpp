cc3c7f22145fac3b34b4be364f37ed2db6e3ea2e
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-01-28 22:14:20 +0300

    More GLM headers included

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include <GLM/glm.hpp>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <GLM/gtc/type_ptr.hpp>
+#include <GLM/gtc/matrix_transform.hpp>
 
 //TODO: this should be maximum display resolution when the game starts 
 const char *WINDOW_TITLE = "GANGSTA";
