c94376c787f413ba88a2f19d3b2a46ad793efb3a
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-02 15:14:46 +0300

    Only one MVP matrix in shader

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -6,11 +6,9 @@ layout (location = 1) in vec3 vertex_color;
 out vec3 pixel_color;
 
 uniform vec3 offset;
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
+uniform mat4 MVP;
 
 void main() {
-	gl_Position = projection * view * model * vec4(position, 1.0);
+	gl_Position = MVP * vec4(position, 1.0);
 	pixel_color = vertex_color;
 }
