af2ab6c84048fd08048739afe143446d99adc31b
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-03 12:09:41 +0300

    Project, view and model in shader

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -5,9 +5,11 @@ layout (location = 1) in vec3 vertex_color;
 
 out vec3 pixel_color;
 
-uniform mat4 MVP;
+uniform mat4 projection;
+uniform mat4 view;
+uniform mat4 model;
 
 void main() {
-	gl_Position = MVP * vec4(position, 1.0);
+	gl_Position = projection * view * model * vec4(position, 1.0);
 	pixel_color = vertex_color;
 }
